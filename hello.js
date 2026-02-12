a93d7235f7925764b3fac7075efd7af0a189651a
Luca Lambia <luca@lambia.it>
2026-02-12 20:12:09 +0100

    gestione argomenti

--- hello.js
+++ hello.js
@@ -1,4 +1,13 @@
-const linguaggio = "Node.js";
+// console.log(process.argv);
+// process.argv[0] --> eseguibile node
+// process.argv[1] --> il nostro file
+// process.argv[2] --> primo argomento
+// process.argv[...] --> così via
 
-console.log(`Hello ${linguaggio} World`);
+const nome = process.argv[2];
 
+if (nome) {
+	console.log(`Hello ${nome}!`);
+} else {
+	console.error("Errore: non conosco il tuo nome!")
+}
